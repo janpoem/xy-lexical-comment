aeb3b4ca76ac963f6061b47de471f464dc863e36
Jiankai Zeng <janpoem@163.com>
2025-02-25 11:05:52 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css';
+import { Editor } from './Editor';
+
+function App() {
+
+  return (
+    <>
+      <div className={'editor-shell'}>
+        <Editor />
+      </div>
+    </>
+  );
+}
+
+export default App;
